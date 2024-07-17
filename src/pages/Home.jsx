bc45ac34183802cffafc17a0c58e894e3e891e32
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2024-07-17 14:32:23 +0300

    main added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
 import React from "react";
-
+import "./Home.css";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>See All of our works</h1>
+      <ul>
+        <a href="/mado">
+          <img src="mado/screen.png" alt="Mado " />
+          <h2>Mado hotel</h2>
+          <button>Watch Demo</button>
+        </a>
+        <a href="/grandpalaceaddis">
+          <img src="grand/screen.png" alt="Mado " />
+          <h2>The Grand Place</h2>
+          <button>Watch Demo</button>
+        </a>
+        <a href="/harmony-hotel">
+          <img src="harmony/screen.png" alt="Mado " />
+          <h2>Harmoney Hotel</h2>
+          <button>Watch Demo</button>
+        </a>
+        <a href="/interluxuryhotel">
+          <img src="inter/screen.png" alt="Mado " />
+          <h2>Inter Lexury Hotel</h2>
+          <button>Watch Demo</button>
+        </a>
+      </ul>
+    </div>
+  );
 }
